112f2bd13b87ee857832ce5e2b7a93b36457dd69
Krishnamohan Yerrabilli <krishnamohan72452964@gmail.com>
2022-10-21 14:34:56 +0530

    updated script

--- Server Setup/K8s-Cluster-Provision-Setup/docker-script.sh
+++ Server Setup/K8s-Cluster-Provision-Setup/docker-script.sh
@@ -1,9 +1,15 @@
-sudo apt-get update
+sudo apt update
 
-sudo apt install docker.io
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
 
-sudo systemctl start docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-sudo systemctl enable docker
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 
-docker --version
+sudo apt update
+
+apt-cache policy docker-ce
+
+sudo apt install docker-ce
+
+sudo systemctl status docker
